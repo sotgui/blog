e60149854ac77a455cc14e91c998ad70f6e31262
sotgui <gus3779@gmail.com>
2022-10-27 17:28:21 +0900

    20221027 JHLEE modified Modal

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ import './App.css';
 function App() {
 
   let [title,changeTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학'])
-  let [title1,changeTitle1] = useState('남자 코트 추천')
-  let [title2,changeTitle2] = useState('강남 우동 맛집')
-  let [title3,changeTitle3] = useState('파이썬 독학')
-  let posts = '강남 우동 맛집'
   let [like, changeLike] = useState([0, 0, 0])
   let [modal, setModal] = useState(false);
+  let [titleNum, setTitleNum] = useState(0);
 
   return (
     <div className="App">
@@ -20,14 +17,15 @@ function App() {
       {title.map(function(a,i){
         return (
           <div className='list' key={i}>
-            <h4 onClick={ () => {setModal(!modal);} }> {a}  </h4>
+            <h4 onClick={ () => {
+                setModal(!modal);
+                setTitleNum(i); } }> {a}  </h4>
             <h4>
               <span onClick={ ()=> {
                 let copy = [...like];
-                copy[i] = copy[i] + 1; 
+                copy[i] = copy[i] + 1;
                 changeLike(copy)} }> 👍 </span> {like[i]} 
-            </h4>    
-            {modal == true ? <Modal title={a}></Modal> : null}
+            </h4>   
             <p>2월 7일 발행</p>
             <button onClick={ ()=> {
               let copy = [...title];  
@@ -36,32 +34,9 @@ function App() {
               }}> 수정 </button>
           </div>
         )
-      })}
-      {/* <div className="list">
-        <h4 onClick={ () => {setModal(!modal);} }>{title[0]}  <span onClick={ ()=> {changeLike(like + 1)} }>👍</span> {like} </h4>
-        <p>2월 17일 발행</p>
-        <button onClick={ ()=> {
-          let copy = [...title];  // 그냥 let copy = title 하면 같은 주소를 가리키기 때문에 값복사해줌 (deep copy) (점 세개 : spread operator. ex>...[1,2,3] == 1,2,3)
-          copy[0] = '여자 코트 추천';
-          changeTitle(copy)       // 주소복사 하는 경우 copy = title 이어서 화면 변경 안됨
-          }}> 수정 </button>
-      </div>
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <button onClick={ ()=> {
-          let copy = [...title];
-          copy.sort();
-          changeTitle(copy)
-          }}> 정렬 </button>
-      </div>
-      {modal == true ? <Modal></Modal> : null} */}
+      })} 
+      {modal == true ? <Modal title={title} titleNum={titleNum}></Modal> : null}
+      
     </div>
   );
 }
@@ -69,7 +44,7 @@ function App() {
 function Modal(props){
   return(
     <div className='modal' style={{ background : props.color }}>
-      <h4>{props.title}</h4>
+      <h4>{props.title[props.titleNum]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
